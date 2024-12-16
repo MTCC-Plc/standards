b9cf0816731c3c382b8d0629fb18502afaa09077
mismaah <mismaahabdulla@gmail.com>
2024-12-16 10:22:46 +0500

    feat(logger): allow additional contexts to be ignored in StandardLogger

--- dist/libs/logger/index.d.ts
+++ dist/libs/logger/index.d.ts
@@ -1,13 +1,16 @@
 import { ConsoleLogger } from "@nestjs/common";
 /**
- * A custom logger that disables all logs emitted by calling `log` method if
- * they use one of the following contexts:
+ * A custom logger that disables all logs emitted by calling `log` method.
+ * By default ignores the following contexts:
  * - `InstanceLoader`
  * - `RoutesResolver`
  * - `RouterExplorer`
  * - `NestFactory`
+ *
+ * To ignore additional contexts, pass them as an array to the constructor.
  */
 export declare class StandardLogger extends ConsoleLogger {
     static contextsToIgnore: string[];
+    constructor(additionalContextsToIgnore?: string[]);
     log(_: any, context?: string): void;
 }
--- dist/libs/logger/index.js
+++ dist/libs/logger/index.js
@@ -3,14 +3,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.StandardLogger = void 0;
 const common_1 = require("@nestjs/common");
 /**
- * A custom logger that disables all logs emitted by calling `log` method if
- * they use one of the following contexts:
+ * A custom logger that disables all logs emitted by calling `log` method.
+ * By default ignores the following contexts:
  * - `InstanceLoader`
  * - `RoutesResolver`
  * - `RouterExplorer`
  * - `NestFactory`
+ *
+ * To ignore additional contexts, pass them as an array to the constructor.
  */
 class StandardLogger extends common_1.ConsoleLogger {
+    constructor(additionalContextsToIgnore) {
+        if (additionalContextsToIgnore) {
+            StandardLogger.contextsToIgnore.push(...additionalContextsToIgnore);
+        }
+        super();
+    }
     log(_, context) {
         if (context && !StandardLogger.contextsToIgnore.includes(context)) {
             super.log(this, ...arguments);
