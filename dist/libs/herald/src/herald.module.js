280921ae796846510968f6274fc01de917383500
mismaah <mismaahabdulla@gmail.com>
2025-09-25 11:05:10 +0500

    fix(herald): ensure useFactory is defined and handle its execution asynchronously

--- dist/libs/herald/src/herald.module.js
+++ dist/libs/herald/src/herald.module.js
@@ -5,6 +5,15 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var HeraldModule_1;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.HeraldModule = void 0;
@@ -26,7 +35,13 @@ let HeraldModule = HeraldModule_1 = class HeraldModule {
         if (options.useFactory) {
             providers.push({
                 provide: herald_service_1.HeraldService,
-                useFactory: options.useFactory,
+                useFactory: (...args) => __awaiter(this, void 0, void 0, function* () {
+                    if (!options.useFactory) {
+                        throw new Error("useFactory is required");
+                    }
+                    const config = yield options.useFactory(...args);
+                    return new herald_service_1.HeraldService(config);
+                }),
                 inject: options.inject || [],
             });
         }
